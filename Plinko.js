2c75ceb7736b232dee2932d8e68ae3f35639c6ac
ishanKasodekar <76422610+ishanKasodekar@users.noreply.github.com>
2021-08-11 13:53:47 +0530

    Update Plinko.js

--- Plinko.js
+++ Plinko.js
@@ -4,11 +4,12 @@ class Plinko {
             restitution: 1,
             friction: 0,
             isStatic:true
-        }
+        
         this.r = 10;
         this.body = Bodies.circle(x, y, this.r, options);
         
         World.add(world, this.body);
+    }
     }
     display() {
 
